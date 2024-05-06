d9a5d3b46ef280c6e6684c96c5961fb37de2c85c
Lucy Bellott <lbellott@tripadvisor.com>
2024-05-06 12:21:27 -0500

    fixed associations

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -5,15 +5,15 @@ const { Card } = require('./Card');
 const { Attack } = require('./Attack');
 
 // Associations
-User.hasMany(Deck)
+User.hasOne(Deck)
 Deck.belongsTo(User)
 
 Deck.hasMany(Card)
 Card.belongsTo(Deck)
 
 
-Card.hasMany(Attack)
-Attack.belongsTo(Card)
+Card.belongsToMany(Attack, { through: 'CardAttack' });
+Attack.belongsToMany(Card, { through: 'CardAttack' });
 
 
 module.exports = { User, Deck, Card, Attack };
